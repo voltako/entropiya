173d3fce266aa0670fd65ad7a4bf7c85869562cc
voltako <54960087+voltako@users.noreply.github.com>
2020-10-16 11:26:53 +0300

    Add files via upload

--- entrop.py
+++ entrop.py
@@ -1,5 +1,16 @@
 import math
 import os
+from collections import Counter
+def entrop():
+    data = file_finder()
+    entropy = 0
+    letters = 0
+    a = list(Counter(data).values())
+    for item in a:
+        freq = item / len(data)
+        log2 = math.log2(freq) * freq
+        entropy = entropy + log2
+    return -entropy
 def file_finder():
     print('введите путь до директории с вашим файлом с расширением .txt.')
     print('пример:/home/user/Desctop/shennon')
@@ -10,27 +21,9 @@ def file_finder():
     file = (input('введите название файла,который вы хотите использовать: '))
     use = (dir+'/'+file)
     print(file)
-    global file_open
     file_open = open(use,'rb')
     file_print = file_open.read()
-    global data
     data = list(file_print)
-    print(data)
-file_finder()
-def entrop():
-    entrop = 0
-    cnt = 0
-    logver = 1
-    a = 0
-    l = len(data)
-    print(l)
-    set_list = set(data)
-    set_list = (list(set_list))
-    for item in set_list:
-        ver=data.count(item)
-        cnt = ver/l
-        logver = math.log2(cnt) * cnt
-        entrop = entrop+logver
-    print(-entrop)
-    file_open.close()
-entrop()
+    return data
+print(entrop())
+
---
+++ test2.py
@@ -0,0 +1,8 @@
+from collections import Counter
+import math
+import os
+def elem(message):
+    return (list(Counter().values()))
+def item(message):
+    return (for message in elem)
+print(item())
